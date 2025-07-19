45cf2a3a76761ce006522dd56c4904cb624310b2
demetriotorgan <demetriotorgan@hotmail.com>
2025-07-19 11:40:54 -0300

    deleteRoute03

--- routes/registroRoutes.js
+++ routes/registroRoutes.js
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post('/save', saveRegistro);
 router.get('/all', getRegistros);
-router.delete('/delete:id', deleteRegistro)
+router.delete('/delete/:id', deleteRegistro)
 
 module.exports = router;
